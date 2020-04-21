ff659ecae4b47bdafc8ff01171c2590ea1208bf8
Mohamad Fadil <xymerz1@gmail.com>
2020-04-21 19:03:50 +0800

    Added questions

--- quiz/src/components/Header.vue
+++ quiz/src/components/Header.vue
@@ -1,3 +1,8 @@
 <template>
-    <h1>Header</h1>
+    <div>
+        <b-nav tabs>
+            <b-nav-item disable><b>Fancy Quiz App</b></b-nav-item>
+            <b-nav-item disable>Counter: 4/10</b-nav-item>
+        </b-nav>
+    </div>
 </template>
